322a779d5f682e5bc774a1762069fc88fa036ee8
Prateek Shourya <prateekshourya29@gmail.com>
2021-09-18 22:47:59 +0530

    Add functions solution 9.

---
+++ solutions/functions/Ques9.java
@@ -0,0 +1,23 @@
+package solutions.functions;
+
+import java.util.Scanner;
+
+public class Ques9 {
+  public static int factorial(int num) {
+    if (num < 0) return -1;
+    if (num == 1 || num == 0) return 1;
+    return num * factorial(num - 1);
+  }
+
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    System.out.println("Enter a number: ");
+    int num = in.nextInt();
+    int factorial_value = factorial(num);
+
+    if (factorial_value == -1) System.out.println("Please enter a positive value");
+    else System.out.println("Factorial is: " + factorial_value);
+
+    in.close();
+  }
+}
