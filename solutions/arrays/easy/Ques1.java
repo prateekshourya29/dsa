d8519113d487c42a5221b78c3909e7d2cc560efa
Prateek Shourya <prateekshourya29@gmail.com>
2021-09-19 21:28:57 +0530

    Add arrays easy solution 1.

---
+++ solutions/arrays/easy/Ques1.java
@@ -0,0 +1,12 @@
+package solutions.arrays.easy;
+
+public class Ques1 {
+  // [Build Array from Permutation](https://leetcode.com/problems/build-array-from-permutation/)
+  public int[] buildArray(int[] nums) {
+    int[] ans = new int[nums.length];
+    for (int i = 0; i < nums.length; i++) 
+      ans[i] = nums[nums[i]];
+
+    return ans; 
+  }
+}
