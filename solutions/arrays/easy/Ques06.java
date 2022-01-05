37eb05d3c548778b515555ac9db64c0b4535ec7b
Prateek Shourya <prateekshourya29@gmail.com>
2022-01-05 16:57:28 +0530

    Add arrays easy solution 6.

---
+++ solutions/arrays/easy/Ques06.java
@@ -0,0 +1,20 @@
+package solutions.arrays.easy;
+
+public class Ques06 {
+  // [Kids With the Greatest Number of Candies](https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/)
+  public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+    ArrayList<Boolean> ans = new ArrayList<>();
+    int max = candies[0];
+    for (int i = 1; i < candies.length; i++) {
+        if (candies[i] > max) max = candies[i];
+    }
+    for (int i = 0; i < candies.length; i++) {
+        if (candies[i] + extraCandies >= max) {
+            ans.add(true);
+            continue;
+        }
+        ans.add(false);
+    }
+    return ans;
+  }
+}
