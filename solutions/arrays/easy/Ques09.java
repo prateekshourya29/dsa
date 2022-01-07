b4d95a5d62e8f5c0780b7f86de0700f544128a8c
Prateek Shourya <prateekshourya29@gmail.com>
2022-01-07 19:29:15 +0530

    Add arrays easy solution 9.

---
+++ solutions/arrays/easy/Ques09.java
@@ -0,0 +1,25 @@
+package solutions.arrays.easy;
+
+public class Ques09 {
+  // [Create Target Array in the Given Order](https://leetcode.com/problems/create-target-array-in-the-given-order/)
+  class Solution {
+    public int[] createTargetArray(int[] nums, int[] index) {
+        int[] target = new int[nums.length];
+        Arrays.fill(target, -1); // Set each element to -1. Since 0 <= nums[i] <= 100
+        for (int i = 0; i < nums.length; i++) {
+            if (target[index[i]] != -1) {
+                // If the index is already occupied find the first empty index shift all the values one right and then fill that index.
+                int j = index[i];
+                while (target[j] != -1) {
+                    j++;
+                }
+                for (; j > index[i]; j--) {
+                    target[j] = target[j-1];
+                }
+            }
+            target[index[i]] = nums[i];
+        }
+        return target;
+    }
+  }
+}
