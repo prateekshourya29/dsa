e281e7474f733bb0af4477eaac2234b861bee45c
Prateek Shourya <prateekshourya29@gmail.com>
2022-01-07 18:48:46 +0530

    Add arrays easy solution 8.

---
+++ solutions/arrays/easy/Ques08.java
@@ -0,0 +1,18 @@
+package solutions.arrays.easy;
+
+public class Ques08 {
+  // [How Many Numbers Are Smaller Than the Current Number](https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/)
+  class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] arr = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            int lower_num_count = 0;
+            for(int j = 0; j < nums.length; j++) {
+                if(nums[j] < nums[i]) lower_num_count++;
+            }
+            arr[i] = lower_num_count;
+        }
+        return arr;
+    }
+  }
+}
