ece1840eb9b0b0504b5be2092c640dab2c751797
Prateek Shourya <prateekshourya29@gmail.com>
2022-01-12 22:44:06 +0530

    Add arrays easy solution 19.

---
+++ solutions/arrays/easy/Ques19.java
@@ -0,0 +1,23 @@
+package solutions.arrays.easy;
+
+public class Ques19 {
+  // [Maximum Population Year](https://leetcode.com/problems/maximum-population-year/)
+  class Solution {
+    public int maximumPopulation(int[][] logs) {
+        int[] arr = new int[100];
+        for(int[] log: logs) {
+            for(int i=log[0]; i < log[1]; i++)
+                arr[i-1950]++;
+        }
+        int index = 0;
+        int count = 0; 
+        for(int i = 0; i < 100; i++) {
+            if(arr[i] > count) {
+                count = arr[i];
+                index = i;
+            }
+        }
+        return index + 1950;
+    }
+  }
+}
