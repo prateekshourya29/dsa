e87336b6fd2c17af21bfc4f74aebf96bfb3c9acf
Prateek Shourya <prateekshourya29@gmail.com>
2022-03-05 10:34:10 +0530

    Add searching easy solution 1.

---
+++ solutions/searching/easy/Ques01.java
@@ -0,0 +1,33 @@
+package solutions.searching.easy;
+
+import java.util.Scanner;
+
+public class Ques01 {
+  public static int mySqrt(int x) {
+    long start = 0;
+    long end = x;
+    long ans = 0;
+    
+    while (start <= end) {
+      long mid = start + (end - start) / 2;
+      
+      if (mid * mid == x) return (int)mid;
+      else if (mid * mid > x) end = mid - 1;
+      else {
+        start = mid + 1;
+        ans = mid;
+      }
+      
+    }
+    
+    return (int)ans;
+  }
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+
+    int x = in.nextInt();
+    System.out.println(mySqrt(x));
+
+    in.close();
+  }
+}
