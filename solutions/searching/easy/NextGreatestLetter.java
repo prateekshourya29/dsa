963e77573a18cec03472ac4028ee003622a70d41
Prateek Shourya <prateekshourya29@gmail.com>
2022-03-09 23:59:44 +0530

    Add searching easy solution NextGreatestLetter.

---
+++ solutions/searching/easy/NextGreatestLetter.java
@@ -0,0 +1,26 @@
+package solutions.searching.easy;
+
+public class NextGreatestLetter {
+  public static char nextGreatestLetter(char[] letters, char target) {
+      int start = 0;
+      int end = letters.length - 1;
+    
+      while (start <= end) {
+        int mid = start + (end - start) / 2;
+        if (letters[mid] == target) {
+          start = mid + 1;
+          break;
+        } else if (letters[mid] > target) {
+          end = mid - 1;
+        } else {
+          start = mid + 1;
+        }
+      }
+    
+    return letters[start % letters.length];
+  }
+
+  public static void main(String[] args) {
+    System.out.println(nextGreatestLetter(new char[] {'c','f','j'}, 'k'));
+  }
+}
