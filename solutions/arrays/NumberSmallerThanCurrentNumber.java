e23283f6064e141bdd4098433b7d45660815cf5b
Prateek Shourya <prateekshourya29@gmail.com>
2022-04-03 14:04:47 +0530

    Update `How Many Numbers Are Smaller Than the Current Number` solution.

--- solutions/arrays/NumberSmallerThanCurrentNumber.java
+++ solutions/arrays/NumberSmallerThanCurrentNumber.java
@@ -1,16 +1,32 @@
 package solutions.arrays;
 
+import java.util.Arrays;
+
 // [How Many Numbers Are Smaller Than the Current Number](https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/)
 public class NumberSmallerThanCurrentNumber {
-  public int[] smallerNumbersThanCurrent(int[] nums) {
-      int[] arr = new int[nums.length];
-      for (int i = 0; i < nums.length; i++) {
-          int lower_num_count = 0;
-          for(int j = 0; j < nums.length; j++) {
-              if(nums[j] < nums[i]) lower_num_count++;
-          }
-          arr[i] = lower_num_count;
-      }
-      return arr;
-  }
+	public int[] smallerNumbersThanCurrent(int[] nums) {
+			int n = nums.length;
+			int[] arr = nums.clone();
+			Arrays.sort(arr);
+			
+			int[] ans = new int[n];
+			int i = 0;
+			for (int a: nums) {
+				int start = 0;
+				int end = n - 1;
+				
+				while (start <= end) {
+					int mid = start + (end - start) / 2;
+					if (arr[mid] >= a) {
+						end = mid - 1;
+					} else {
+						start = mid + 1;
+					}
+				}
+				
+				ans[i++] = start;
+			}
+			
+			return ans;
+		}
 }
