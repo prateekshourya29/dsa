774d99818f669a29f62d9185e395790c0530a4aa
Prateek Shourya <prateekshourya29@gmail.com>
2022-07-05 12:07:15 +0530

    Add `N-Queens` solution.

---
+++ solutions/recursion/NQueen.java
@@ -0,0 +1,70 @@
+package solutions.recursion;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+// https://leetcode.com/problems/n-queens/
+public class NQueen {
+  static HashSet<Integer> col = new HashSet<>();
+  static HashSet<Integer> posDiag = new HashSet<>(); // r + c
+  static HashSet<Integer> negDiag = new HashSet<>(); // r - c
+
+  public static boolean isValid(List<String> board, int r, int c, int n) {
+    if (col.contains(c)) return false;
+    if (posDiag.contains(r + c)) return false;
+    if (negDiag.contains(r - c)) return false;
+
+    return true;
+  }
+
+
+  public static List<List<String>> solve(int r, int n, List<String> board) {
+    List<List<String>> ans = new ArrayList<>();
+    if (r == n) {
+      List<String> b =  new ArrayList<>(board);
+      ans.add(b);
+      return ans;
+    }
+
+    for (int i = 0; i < n; i++) {
+      if (isValid(board, r, i, n)) {
+        String old = board.get(r);
+        String s = board.get(r).substring(0, i) + 'Q' + board.get(r).substring(i + 1);
+        board.remove(r);
+        board.add(r, s);
+        col.add(i);
+        posDiag.add(r + i);
+        negDiag.add(r - i);
+        ans.addAll(solve(r + 1, n, board));
+        board.remove(r);
+        board.add(r, old);
+        col.remove(i);
+        posDiag.remove(r + i);
+        negDiag.remove(r - i);
+      }
+    }
+
+    return ans;
+  }
+
+  public static List<List<String>> solveNQueens(int n) {
+    List<String> board = new ArrayList<>();
+    String str = "";
+
+    for (int i = 0; i < n; i++) {
+      str += ".";
+    }
+
+    for (int i = 0; i < n; i++) {
+      board.add(String.valueOf(str));
+    }
+
+    return solve(0, n, board);
+  }
+
+  public static void main(String[] args) {
+    int n = 4;
+    System.out.println(solveNQueens(n));
+  }
+}
