e9761bba4396f592a75468be8517fea7333956ce
Prateek Shourya <prateekshourya29@gmail.com>
2022-07-10 12:39:03 +0530

    Add `Linked List Cycle` solution.

---
+++ solutions/linkedlist/LinkedListCycle.java
@@ -0,0 +1,19 @@
+package solutions.linkedlist;
+
+// https://leetcode.com/problems/linked-list-cycle/
+public class LinkedListCycle {
+  public boolean hasCycle(ListNode head) {
+    ListNode fast = head;
+    ListNode slow = head;
+    
+    while (fast != null && fast.next != null) {
+      fast = fast.next.next;
+      slow = slow.next;
+      if (fast == slow) {
+        return true;
+      }
+    }
+    
+    return false;
+  }
+}
