9a3e6bc3391dd4e5d41acb48773f84a2c8080d44
Prateek Shourya <prateekshourya29@gmail.com>
2022-07-10 13:21:30 +0530

    Add `Middle of the Linked List` solution.

---
+++ solutions/linkedlist/MiddleOfLinkedList.java
@@ -0,0 +1,16 @@
+package solutions.linkedlist;
+
+// https://leetcode.com/problems/middle-of-the-linked-list/
+public class MiddleOfLinkedList {
+  public ListNode middleNode(ListNode head) {
+    ListNode slow = head;
+    ListNode fast = head;
+    
+    while (fast != null && fast.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+    
+    return slow;
+  }
+}
