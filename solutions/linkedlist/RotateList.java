c47adc59ad8d2fadb29cbed5a745a845b54d1271
Prateek Shourya <prateekshourya29@gmail.com>
2022-07-15 14:06:51 +0530

    Add `Rotate List` solution.

---
+++ solutions/linkedlist/RotateList.java
@@ -0,0 +1,90 @@
+package solutions.linkedlist;
+
+// https://leetcode.com/problems/rotate-list/
+public class RotateList {
+	// Simple Solution
+	public ListNode rotateRight(ListNode head, int k) {
+    if (head == null || head.next == null) {
+      return head;
+    }
+    
+    int n = 0;
+    ListNode temp = head;
+    while (temp != null) {
+      temp = temp.next;
+      n++;
+    }
+    
+    k = k % n;
+    
+    while (k-- > 0) {
+      ListNode end = head;
+      ListNode prev = null;
+      
+      while (end != null && end.next != null) {
+        prev = end;
+        end = end.next;
+      }
+      
+      prev.next = null;
+      end.next = head;
+      head = end;
+    }
+    
+    return head;
+  }
+
+	// Using Reverse  
+  public ListNode rotateRightUsingReverse(ListNode head, int k) {
+    if (head == null || head.next == null || k == 0) {
+      return head;
+    }
+    
+    int n = 1;
+    ListNode start = head;
+    ListNode end = head;
+    
+    while (end.next != null) {
+      end = end.next;
+      n++;
+    }
+    
+    k = k % n;
+    
+    if (k == 0) return head;
+    
+    head = reverseList(start, end);
+    
+    ListNode first = end;
+    ListNode second = start;
+    ListNode firstEnd = first;
+    ListNode secondStart = null;
+    
+    n = 1;
+    while (n < k) {
+      firstEnd = firstEnd.next;
+      n++;
+    }
+    secondStart = firstEnd.next;
+    
+    head = reverseList(first, firstEnd);
+    first.next = reverseList(secondStart, second);
+    
+    return head;
+  }
+
+	public ListNode reverseList(ListNode start, ListNode end) {
+    ListNode prev = end.next;
+    end.next = null;
+    ListNode curr = start;
+
+    while (curr != null) {
+      ListNode next = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = next;
+    }
+
+    return prev;
+  }
+}
